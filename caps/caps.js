b9f559d65b2019203d11276d9b5632908793073c
Nedal_Erekat <nedalerekat@gmail.com>
2020-09-29 17:57:04 +0300

    server socket

--- caps/caps.js
+++ caps/caps.js
@@ -4,37 +4,61 @@
  * accept all inbound events and data,validate them
  * broadcast to everyone except the sender
  */
-const net = require('net')
-const server = net.createServer()
+// const net = require('net')
+// const server = net.createServer()
 const port = process.env.PORT || 4000;
-server.listen(port, () => console.log(`server is running on ${port}`));
+const io=require('socket.oi')(port)
 
-let socketPool = {};
+io.on('connection',(socket)=>{
+    console.log("CAPS Connection!")
+})
+let currentRoom='';
+const caps = io.of('/caps')
+caps.on('join',room=>{
+    caps.leave(currentRoom);
+    caps.join(room);
 
-server.on('connection', (socket) => {
-    console.log("CAPS is online!!!");
-    const id = `Socket-${Math.random()}`;
-    socketPool[id] = socket;
-    socket.on('data', buffer => {
-        // encoded buffer
-        // console.log("buffer >>>> ", buffer);
-        let msg = JSON.parse(buffer.toString());
-        console.log("msg >>> ", msg);
-        broadcast(msg);
-        
+    caps.on('pickup', payload=>{
+        caps.to(currentRoom).emit('pickup',payload)
+        // caps.broadcast.emit('pickup',payload)
+
     });
-});
-
-server.on('error', (e)=> {
-    console.log("ERROR !!!!!!! ", e)
+    caps.on('in-transit', payload=>{
+        caps.to(currentRoom).emit('in-transit',payload)
+    });
+    caps.on('delivered', payload=>{
+        caps.to(currentRoom).emit('delivered',payload)
+    });
 })
 
-function broadcast(msg) {
-    let payload = JSON.stringify(msg);
-    for (let id in socketPool) {
-        socketPool[id].write(payload);
-    }
-}
+// server.listen(port, () => console.log(`server is running on ${port}`));
+
+// let socketPool = {};
+
+// server.on('connection', (socket) => {
+//     console.log("CAPS is online!!!");
+//     const id = `Socket-${Math.random()}`;
+//     socketPool[id] = socket;
+//     socket.on('data', buffer => {
+//         // encoded buffer
+//         // console.log("buffer >>>> ", buffer);
+//         let msg = JSON.parse(buffer.toString());
+//         console.log("msg >>> ", msg);
+//         broadcast(msg);
+        
+//     });
+// });
+
+// server.on('error', (e)=> {
+//     console.log("ERROR !!!!!!! ", e)
+// })
+
+// function broadcast(msg) {
+//     let payload = JSON.stringify(msg);
+//     for (let id in socketPool) {
+//         socketPool[id].write(payload);
+//     }
+// }
 
 
 
--- vendor/vendor.js
+++ vendor/vendor.js
@@ -3,39 +3,46 @@ require('dotenv').config()
 var faker = require('faker');
 
 //Connect to the CAPS server
-const net = require('net');
-const vendor = new net.Socket();
+// const net = require('net');
+// const vendor = new net.Socket();
 
 const storeName = process.env.store_name;
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 4000;
+const io = require('socket.io-client')
+const vendor = io.connect('http://localhost:3000/caps');
+vendor.on('connect', () => {
 
-vendor.connect(port, host, () => {
-    console.log("Vendor connecting ... ")
-});
-vendor.on('close', function () {
-    console.log("connection is closed!!");
-});
+    vendor.emit('join', 'venderRoom')
 
+    vendor.on('delivered', payload => {
+        console.log(`thank you for delivering ${payload.orderId}`, new Date());
+    })
 
-vendor.on('data', (data) => {
-    let jsonData = JSON.parse(data);
-    if (jsonData.event === 'delivered') {
-        console.log(`thank you for delivering ${jsonData.payload.orderId}`,new Date());
-    }
-});
+    setInterval(() => {
 
-setInterval(() => {
-    let msg = {
-        event: "pickup",
-        payload: {
+        let payload = {
             storeName: storeName,
             orderId: faker.random.uuid(),
             customerName: faker.name.findName(),
             address: faker.address.country()
         }
-    }
-    vendor.write(JSON.stringify(msg))
-}, 30000);
+        vendor.emit("pickup", payload)
+        write(JSON.stringify(msg))
+    }, 30000);
+})
+
+// vendor.connect(port, host, () => {
+//     console.log("Vendor connecting ... ")
+// });
+// vendor.on('close', function () {
+//     console.log("connection is closed!!");
+// });
 
 
+// vendor.on('data', (data) => {
+//     let jsonData = JSON.parse(data);
+//     if (jsonData.event === 'delivered') {
+//         console.log(`thank you for delivering ${jsonData.payload.orderId}`,new Date());
+//     }
+// });
